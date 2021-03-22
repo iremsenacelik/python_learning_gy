ed32bf4f2568c1c9a7b1085eaf3a7800a97b0268
iremsenacelik <iremsbet13@gmail.com>
2021-03-22 21:27:42 +0300

    variables , types and ?function fundamentals.

---
+++ python_101/variables.py
@@ -0,0 +1,34 @@
+#Variables
+
+a = 999
+b = 'hello_python'
+c = a*5
+
+type(100)
+type(100.1)
+type(1 + 2j)
+
+#type dönüşümleri
+
+'''kullanıcıdan gelen ifadeler default 
+olarak string olarak bize gelir.'''
+
+first_number = input()
+second_number = input()
+type(first_number)
+first_number + second_number
+
+
+int(first_number) + int(second_number)
+
+float(25)
+str(12)
+
+
+#print()
+
+print('irem ' , 'sena' , ' celik')
+print('irem ' , 'sena' , ' celik' , sep='_')
+print()
+?print 
+'''fonksiyon ile ilgili detaylı bilgiyi konsolda görürüz.'''
