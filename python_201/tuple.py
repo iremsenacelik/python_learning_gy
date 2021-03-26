9bfee0b3180d601175713dab76899c514147a18d
iremsenacelik <iremsbet13@gmail.com>
2021-03-26 11:18:33 +0300

    tuple notes

---
+++ python_201/tuple.py
@@ -0,0 +1,37 @@
+#VERİ YAPILARI - TUPLE
+
+#tuple(demet ) oluşturma.
+''' 
+tuple da liste gibi bir veri yapısıdır.
+tuple'ların listelerden en önemli farkı ; değiştirilemez olmasıdır.
+() kullanılarak tuple oluşturulabilir.
+Veya tuple() fonksiyonu kullanılabilir.
+Tek elemalı bir tuple oluştururken elemanın sonuna "," koymak gerekir.
+'''
+
+t = ('i' , 'r' , 5 ,87 , [ 2, 3, 4])
+
+t2 = 'e' , 'm' , 5 ,87 , [ 2, 3, 4]
+
+t3 = ('eleman')
+type(t3)
+
+t4 = ('eleman' ,)
+type(t4)
+
+#Tuple eleman işlemleri
+
+'''
+verilere erişim işlemleri listelerle aynı şekilde yapılır.
+t[1] gibi.
+'''
+
+
+
+
+
+
+
+
+
+
